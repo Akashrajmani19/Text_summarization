c9e6862e5b4e9dbaf04a8364dcd1af77fefb37e9
Akash vaishnav <aakashrankawat19@gmail.com>
2024-03-03 23:29:26 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Text_Summarization',
+    version = '0.0.1',
+    author = 'Akash Vaishnav',
+    author_email = 'akash.vaishnav1908@gmail.com',
+    packages = find_packages(),
+    url = 'https://github.com/Akash-Vaishnav/Text_Summarization',
+    license = 'Apache License 2.0',
+    description = 'Text Summarization'
+)
